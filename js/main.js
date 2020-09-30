f566628cf7f0022f8c96fa06b71e5d86df57a732
Magdalena <polakowska.mag@gmail.com>
2020-09-30 08:52:35 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
 /********** On scroll move welcoming text *********/
-$(document).ready(function(){
-    $(window).scroll(function() {
-        $( ".welcoming_text" ).animate({'margin-bottom': '550px'}, 'slow');
-    });
-});
+// $(document).ready(function(){
+//     $(window).scroll(function() {
+//         $( ".welcoming_text" ).animate({'margin-bottom': '125%'}, 'slow');
+//     });
+// });
 
 function openNav() {
     document.querySelector(".nav_opened").style.right = "0px";
